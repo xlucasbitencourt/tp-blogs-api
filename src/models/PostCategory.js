1caeacfe443cca66ca54fcca30345231ff1383e4
Lucas Bitencourt <lblukz@aol.com>
2022-09-27 22:04:05 -0300

    Requisito 11 concluído

---
+++ src/models/PostCategory.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true },
+  },
+  {
+    timestamps: false,
+    tableName: 'post_categories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      as: 'blogPost' 
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      foreignKey: 'postId',
+      as: 'category' 
+    });
+  };
+
+  return PostCategory;
+};
